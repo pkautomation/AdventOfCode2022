63b85fa93fedf58feb3d77b26adcc69a8e2c12b9
Pawel Kreski <Pawel.Kreski@hyland.com>
2022-12-02 18:39:50 +0100

    day 1

---
+++ AdventOfCode2022/Day1/Program.cs
@@ -0,0 +1,63 @@
+﻿using SharedUtilities;
+
+var puzzle = FileReader.ReadFile();
+
+Console.WriteLine(GetMax());
+var resultsPerElf = GetEach();
+Console.WriteLine(GetMaxResOfElves(resultsPerElf, 3));
+
+int GetMax()
+{
+    int maxRes = 0;
+    int currentCounter = 0;
+
+    foreach (var line in puzzle)
+    {
+        if (line.Length == 0)
+        {
+            if (currentCounter > maxRes)
+            {
+                maxRes = currentCounter;
+            }
+
+            currentCounter = 0;
+        }
+        else currentCounter += int.Parse(line);
+    }
+
+    return maxRes;
+}
+
+List<int> GetEach()
+{
+    var list = new List<int>();
+    int currentCounter = 0;
+
+    foreach (var line in puzzle)
+    {
+        if (line.Length == 0)
+        {
+            list.Add(currentCounter);
+
+            currentCounter = 0;
+        }
+        else currentCounter += int.Parse(line);
+    }
+
+    return list;
+}
+
+int GetMaxResOfElves(List<int> list, int recordsToSum)
+{
+    list.Sort();
+    list.Reverse();
+
+    var res = 0;
+
+    for (int i = 0; i < recordsToSum; i++)
+    {
+        res += list[i];
+    }
+
+    return res;
+}
---
+++ AdventOfCode2022/SharedUtilities/FileReader.cs
@@ -0,0 +1,19 @@
+﻿namespace SharedUtilities
+{
+        public class FileReader
+        {
+            public static List<string> ReadFile(string fileName = "puzzle.txt")
+            {
+                var projectName = System.Reflection.Assembly.GetEntryAssembly().GetName().Name;
+                string[] lines = File.ReadAllLines(@$"C:\repos\AdventOfCode2022\AdventOfCode2022\{projectName}\{fileName}");
+                var values = new List<string>();
+
+                foreach (var line in lines)
+                {
+                    values.Add(line);
+                }
+
+                return values;
+            }
+        }
+}
