2d99180b06b1d28c12dbb460b8bb956d67df6ec0
Pawel Kreski <pawel.kreski@yahoo.com>
2022-12-06 19:44:49 +0100

    day 6

---
+++ AdventOfCode2022/Day6/Program.cs
@@ -0,0 +1,35 @@
+﻿using SharedUtilities;
+
+var puzzle = FileReader.ReadFile().ToArray()[0];
+
+var result = 0;
+
+// i was superlazy this time. this is part 2 only
+for (int i = 14; i < puzzle.Length; i++)
+{
+    var substring = puzzle.Substring(i - 14, 14);
+    var sorted = new string(substring.OrderBy(x => x).ToArray());
+
+    if (sorted[0] != sorted[1] &&
+        sorted[1] != sorted[2] &&
+        sorted[2] != sorted[3] &&
+        sorted[3] != sorted[4] &&
+        sorted[4] != sorted[5] && 
+        sorted[5] != sorted[6] && 
+        sorted[6] != sorted[7] && 
+        sorted[7] != sorted[8] && 
+        sorted[8] != sorted[9] && 
+        sorted[9] != sorted[10]&& 
+        sorted[10] != sorted[11]&& 
+        sorted[11] != sorted[12]&&
+        sorted[12] != sorted[13]
+        )
+    {
+        result = i + 1;
+        break;
+    }
+}
+
+Console.WriteLine(result);
+
+int x = 0;
